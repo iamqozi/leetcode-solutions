3ed63fe9219c6be429a4b71f6051fdb8bbbd27d7
iamqozi <qd31237@seeu.edu.mk>
2026-09-06 19:34:20 +0200

    Add arrays/0001-two-sum/solution.cpp

---
+++ arrays/0001-two-sum/solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // TODO: Find two distinct indices whose values sum to target.
+        // This is an unfinished starter, not an accepted solution.
+        return {};
+    }
+};
